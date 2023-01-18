796aba1b237c2cf8d0cd28cb9471c81e4c4ff02d
Angi Marliu Casella <acasella@danielcastelao.org>
2023-01-18 14:09:03 +0100

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola, soy Angi y estoy creando el proyecto");
+
+        System.out.println("Sigo trabajando en el proyecto y hago un segundo commit");
+
     }
 }
